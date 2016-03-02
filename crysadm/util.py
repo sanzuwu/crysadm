2abef389eb0f00b00793ff72c1352bc383055377
sanzuwu <sanzuwu@gmail.com>
2016-03-02 09:40:15 +0800

    tixian

--- crysadm/util.py
+++ crysadm/util.py
@@ -2,7 +2,7 @@ __author__ = 'powergx'
 import hashlib
 from flask import session
 
-# Éú³ÉhashÃÜÂë
+# ç”Ÿæˆhashå¯†ç 
 def hash_password(pwd):
     """
         :param pwd: input password
@@ -12,13 +12,13 @@ def hash_password(pwd):
 
     return hashlib.md5(str("%s%s" % (app.config.get("PASSWORD_PREFIX"), pwd)).encode('utf-8')).hexdigest()
 
-# Éú³É md5 16Î»Ğ¡Ğ´ÃÜÂë
+# ç”Ÿæˆ md5 16ä½å°å†™å¯†ç 
 def md5(s):
     import hashlib
 
     return hashlib.md5(s.encode('utf-8')).hexdigest().lower()
 
-# ÌáÈ¡´íÎóĞÅÏ¢
+# æå–é”™è¯¯ä¿¡æ¯
 def get_message():
     err_msg = None
     if session.get('error_message') is not None:
@@ -26,7 +26,7 @@ def get_message():
         session['error_message'] = None
     return err_msg
 
-# ÉèÖÃĞÅÏ¢
+# è®¾ç½®ä¿¡æ¯
 def set_message(message,type='error'):
     if type == 'error':
         session['error_message'] = message
