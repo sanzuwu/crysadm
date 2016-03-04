78b75f4004f30b81aa4c182f44ab1625729d464f
sanzuwu <sanzuwu@gmail.com>
2016-03-04 14:43:48 +0800

    cron.sh

---
+++ cron.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#this is cron start script
+#Author:sanzuwu
+#Date:2016-03-04
+#Desc:add a crontab
+#获得随机数返回值，shell函数里算出随机数后，更新该值
+function random()
+{
+
+    min=$1;
+
+    max=$2-$1;
+
+    num=$(date +%s+%N);
+
+    ((retnum=num%max+min));
+
+    #进行求余数运算即可
+
+    echo $retnum;
+
+    #这里通过echo 打印出来值，然后获得函数的，stdout就可以获得值
+
+    #还有一种返回，定义全价变量，然后函数改下内容，外面读取
+}
+
+#定义变量minutes 为随机数
+minutes=$(retnum 0 59)
+echo "${minutes} * * * * root /app/crysadm/run.sh">>/etc/crontab
+echo ===============starting cron==================
+cron start
+echo =================done!========================
+
+
